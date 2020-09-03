d97dc5b8933456b78a09d3cb0f1df278426c324d
svillemez <65796128+svillemez@users.noreply.github.com>
2020-09-03 00:10:44 -0500

    Update query_questions.sql

--- EmployeeSQL/query_questions.sql
+++ EmployeeSQL/query_questions.sql
@@ -9,8 +9,17 @@ SELECT first_name, last_name, hire_date FROM employees
 	WHERE hire_date > '1985-12-31' and hire_date < '1987-01-01';	
 	
 -- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
-
+SELECT 
 -- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
 
 SELECT e.emp_no, last_name, first_name, d.department FROM employees as e, department as d, employeedept as ed
 	WHERE e.emp_no = ed.emp_no;
+	
+-- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+-- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+-- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+-- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+
